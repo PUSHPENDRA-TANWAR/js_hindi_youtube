90eedde417b96344fad7e1d1649ce950895d2e34
PUSHPENDRA-TANWAR <pushpendratanwar87@gmail.com>
2026-09-09 17:12:46 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,23 @@
+let score = "33asd";
+let score2 = null
+console.log(typeof score);
+
+let inNumber = Number(score); // ham ese mention karke data type change sakte h 
+let inNumber2 = Number(score2)
+
+console.log(typeof inNumber);
+console.log(inNumber);// it print NaN (which mean not a number)
+console.log(inNumber2); // now it print 0
+
+let loggin = 1;
+let booleanloggin = Boolean(loggin)
+console.log(booleanloggin); // output = true 
+
+let somenumer = 33
+let stringNumber = String(somenumer)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+
+
