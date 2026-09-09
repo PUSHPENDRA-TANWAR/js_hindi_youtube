2f4b9f8a32c2385057084298deb4ff554bba2d3d
PUSHPENDRA-TANWAR <pushpendratanwar87@gmail.com>
2026-09-09 20:40:37 +0000

    comparision

---
+++ 01_basic/comparision.js
@@ -0,0 +1,53 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+
+// console.log("2"=1); // not allow  (showing error)
+// console.log("2">1); // not allow   (showing error)
+
+// console.log(null>0);// fale
+// console.log(null==0);// fale
+// console.log(null>=0); // true (null is converted in 0 thats why it show zero)
+
+// console.log(undefined>0);// fale
+// console.log(undefined==0);// fale
+// console.log(undefined>=0) // false
+
+// ***********strict check(not only value it also check data type)****************
+// console.log("2" === 2); // false
+// console.log(3 === 2);   // datatype is same but value is diffrent so false
+
+// const id = Symbol('123')
+// const userid = Symbol('123')
+// console.log(id === userid); // it show false 
+
+
+
+//**********************DATA TYPE SUMMERY ***************************//
+
+// Premitive(call by value)
+// 7 types == Numer,String,Boolean,Null,undefiend,symbol(for use making value unique),BigInt
+
+// Reference (non premitive)
+
+// Array,object,Function
+
+// const heros = ["bheem","shaktiman","superman"]; // array
+
+// {
+//     name: Pushpendra
+//     age : 22
+
+// } // objects
+
+// const myfunction = function(){
+//     console.log("hello world");
+    
+// } // function
+
+
+
+
