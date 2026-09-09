0b8d579cb7470414b4bba7302dc51182f1e0a201
PUSHPENDRA-TANWAR <pushpendratanwar87@gmail.com>
2026-09-09 11:38:05 +0000

    datatypes

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,5 +1,12 @@
 const accountId = 14445 // permanent not chnagable
-let password = "1232221" 
-var accountEmail = "test@gmail.com"
+let password = "1232221" // variable declaration  // we only use let
+var accountEmail = "test@gmail.com"//variable declaration // not use anymore becuse of block scope and functional scope
+accountCity = "jaipur" // it is right but we dont use it .not good practise
+let accountState;
 
+//accountId = 23 // this is not allow we cant change current values
+  password = "131321"
+  accountEmail = "erefw@dwd"
+  accountCity = "alwar"
 console.log(accountId);
+console.table([accountId,password,accountEmail,accountCity,accountState])// table form output milega isse
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat js code as a newer version
+//  alert(3+3) // its not valid in node js ...valid in browser
+
+let name = "Monu"//string
+let age = 22 //integer
+let isloggin = false // boolian
+
+// numer => 2 to the power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null  => standalone value
+// undefined => abhi value assign nhi hui h
+// symbol => uniqueness k liye use hote h 
+
+// object 
+
+console.log(typeof age); // typeof se hum kisi ka bhi type jaan sakte h
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined  
+
