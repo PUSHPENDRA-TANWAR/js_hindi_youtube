bd17828a9c9e9d102ce4e7aa8c2e469029e57530
PUSHPENDRA-TANWAR <pushpendratanwar87@gmail.com>
2026-09-10 16:33:28 +0000

    strings

--- 01_basic/06_string.js
+++ 01_basic/06_string.js
@@ -11,6 +11,29 @@ console.log(gamename.charAt(2)); // t
 console.log(gamename.indexOf("t")); //2 
 
 
+const newString = gamename.substring(0,4)
+console.log(newString); // hite
+
+const anotherString = gamename.slice(-4) // (slice me hum negative values ko reverse print karne k liye use karte h) last for character print
+console.log(anotherString);
+
+const newstringone = "   hitesh   "
+console.log(newstringone);
+console.log(newstringone.trim()); // extra whitespace ko hatane k liye use hota h 
+
+const url = "https://hitesh.com/hitesh%20chaudry"
+console.log(url.replace('%20','-'));  // ('search values','relce by it')
+
+console.log(url.includes('hitesh'));// true // ye question puchta h ki url me hitesh word h ki nhi 
+
+
+const gamename2 = new String("hitesh-dot-com");
+
+console.log(gamename2.split('-'));// [ 'hitesh', 'dot', 'com' ] //it split the string on basis of saprater('-') and make an array 
+
+
+
+
 
 
 
@@ -20,4 +43,3 @@ console.log(gamename.indexOf("t")); //2
 // new way
 
 console.log(`hello my name is ${name} and my repo count is ${repocount}`); // use backticks 
-
