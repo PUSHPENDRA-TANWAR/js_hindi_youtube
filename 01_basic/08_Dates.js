3b66cca545d9a5273d5ae7482e330646c050583b
PUSHPENDRA-TANWAR <pushpendratanwar87@gmail.com>
2026-09-10 19:35:14 +0000

    Dates

---
+++ 01_basic/08_Dates.js
@@ -0,0 +1,32 @@
+
+// let myDate = new Date()
+// console.log(myDate); // 2026-09-10T18:54:11.034Z
+// console.log(myDate.toString()); // Thu Sep 10 2026 18:54:11 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); // Thu Sep 10 2026
+// console.log(myDate.toLocaleDateString()); // 9/10/2026
+// console.log(myDate.toJSON()); // 2026-09-10T19:00:11.879Z
+// console.log(myDate.toLocaleString()); // 9/10/2026, 7:00:11 PM
+
+// let mycreatedate = new Date(2023 , 0 , 23)  // ese create hoti h date 
+// let mycreatedate2 = new Date(2023 , 0 , 23 ,5 ,13) 
+// console.log(mycreatedate.toLocaleDateString());
+// console.log(mycreatedate2.toLocaleString());
+
+ let mycreatedate3 = new Date("2023-04-14")
+// console.log(mycreatedate3.toLocaleString());
+
+let myTimeStamp = Date.now() // ye use hoga room book karne me kisne pahle book kiya etc.
+// console.log(myTimeStamp); // milisecond me ans dega
+// console.log(mycreatedate3.getTime());  // milisecond me ans dega but compare karne k kaam aayega
+
+// console.log(Math.floor(Date.now()/1000)); // current date in second
+
+let newDate = new Date()
+console.log(newDate.getDay()); // there is multiple method available // month k liye getMonth + 1 karege kyuki js me month 0 se start hote h 
+
+ // log ese usully use kar lete h // ${newDate.getDay()} and the date is
+
+
+
+
+
