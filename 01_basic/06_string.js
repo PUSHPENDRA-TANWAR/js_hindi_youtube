588e89600ffe5c8e1e32ac454a46b1a03f83507e
PUSHPENDRA-TANWAR <pushpendratanwar87@gmail.com>
2026-09-10 14:32:20 +0000

    string

---
+++ 01_basic/06_string.js
@@ -0,0 +1,23 @@
+const name = "monu";
+const repocount = 50;
+
+const gamename = new String("hitesh"); // ye bhi ek tarika h string declare karne ka (isme hame h ,i,t,... ki positions pata rahti h ,isme length property bhi milti h )
+
+// methods
+console.log(gamename[0]); // h
+console.log(gamename.length); // 6
+console.log(gamename.toUpperCase()); // HITESH
+console.log(gamename.charAt(2)); // t
+console.log(gamename.indexOf("t")); //2 
+
+
+
+
+
+
+//console.log(name + repocount + " value"); // old way 
+
+// new way
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`); // use backticks 
+
