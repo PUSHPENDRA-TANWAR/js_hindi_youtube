ea2d5d20e2d08ef2f68943981c7039d5cb35438a
PUSHPENDRA-TANWAR <pushpendratanwar87@gmail.com>
2026-09-12 13:34:35 +0000

    object_2

---
+++ 02_basic/04_object.js
@@ -0,0 +1,9 @@
+
+// object singleton
+
+// declare karne me diffrence h  singleton or nonsigleton me bass baaki sab same h
+ 
+const tinderuser = {}  // literals / non singelton object
+//const tinderuser = new Object() // singelton object
+
+console.log(tinderuser);
