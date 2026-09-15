ee7679fd5a1c6c893caf2928a46859d4757f3140
PUSHPENDRA-TANWAR <pushpendratanwar87@gmail.com>
2026-09-15 19:29:11 +0000

    objects

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -1,9 +1,87 @@
 
 // object singleton
 
-// declare karne me diffrence h  singleton or nonsigleton me bass baaki sab same h
- 
-const tinderuser = {}  // literals / non singelton object
-//const tinderuser = new Object() // singelton object
+// const tinderUser = new Object()
+const tinderUser = {}
 
-console.log(tinderuser);
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 } // isme object assign nhi honge balki 1 obj me 2 properties ban jayegi
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // isme merge ho jayege
+
+
+const obj3 = {...obj1, ...obj2} // Ye bhi objects ko merge karta hai.
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+//const instructor = course.courseInstructor // normal way // same result
+const {courseInstructor: instructor} = course // distructing way // use in API,Node.js etc
+
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+    // ye object nhi h kyuki object ka name hota h so ye ek json h
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
