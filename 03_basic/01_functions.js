caa69cd5615cba124780a2024d0c2475a0d85933
PUSHPENDRA-TANWAR <pushpendratanwar87@gmail.com>
2026-09-15 20:37:08 +0000

    function

---
+++ 03_basic/01_functions.js
@@ -0,0 +1,72 @@
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice( val1,val2,...num1){ // ...num1(rest operator)(multiple value assign karege to bhi value dikhayga)
+    return num1                                  // 200,400 chale gaye val1 me or val2 me baaki num1 me print ho jayege
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000)) // [200,400,500,2000]
+
+
+// function me object kaise pass karte h
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
+// direct object  bhi pass kar sakte h 
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000])); // dono same h direct bhi kar sakte h 
