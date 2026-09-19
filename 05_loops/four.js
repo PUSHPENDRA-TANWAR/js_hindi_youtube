b7090a7070136da61879f1091c1fe96c1df14c21
PUSHPENDRA-TANWAR <pushpendratanwar87@gmail.com>
2026-09-19 19:05:47 +0000

    four

---
+++ 05_loops/four.js
@@ -0,0 +1,31 @@
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'C++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+
+// for in loop // objects par ye lgta h 
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+// for in loop alway print key in default // so mention what we want to print
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//     console.log(programming[key]); // console.log(key); // output = 0 1 2 3 4
+// }
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+// output it show nothing
+// for in loop not working in map 
